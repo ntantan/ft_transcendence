cfb93e1fb0d23d697963c2e3d56c5aad0a783d60
borntobesso <soyoung.jung13@gmail.com>
2023-04-20 19:56:02 +0200

    2FA switch

---
+++ client/src/components/user/TwoFactorAuth.vue
@@ -0,0 +1,33 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import { userStore } from "@/stores/user";
+
+export default defineComponent({
+	name: "TwoFactorAuth",
+
+	data() {
+		return {
+			userStore,
+			enable: 'no', // userstore.user.enable2FA
+		};
+	},
+
+});
+</script>
+
+<template>
+	<v-card height="350">
+		<v-card-title>
+			<h2>Two Factor Authentication</h2>
+		</v-card-title>
+		<v-container>
+			<v-row>
+				<v-col>
+					<v-switch v-model="enable" color="indigo" true-value="yes" false-value="no" :label="`enable 2FA: ${enable}`" inset></v-switch>
+				</v-col>
+			</v-row>
+		</v-container>
+	</v-card>
+</template>
+
+<style></style>
