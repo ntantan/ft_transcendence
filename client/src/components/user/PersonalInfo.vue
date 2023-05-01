6609e15bbbb17a9b3fb5c6e30d15d280d1fab3d8
borntobesso <soyoung.jung13@gmail.com>
2023-05-01 16:49:20 +0200

    feature: added username edit input rules

--- client/src/components/user/PersonalInfo.vue
+++ client/src/components/user/PersonalInfo.vue
@@ -12,11 +12,18 @@ export default defineComponent({
             URL: "http://localhost:3000/users/avatar/",
             nameEdit: false,
             tmpName: "",
+            rules: [
+                (v: string) => !!v || "Required",
+                (v: string) => v.length <= 20 || "Must be less than 20 characters",
+                (v: string) => v.length >= 3 || "Must be more than 3 characters",
+                (v: string) => /^[a-zA-Z0-9_]*$/.test(v) || "Only letters, numbers and underscores allowed",
+                //https://regexr.com/
+            ],
         };
     },
 
     methods: {
-        async updateAvatar() {
+        async updateAvatar(): Promise<void> {
             const file = (document.getElementById("avatar") as HTMLInputElement).files[0];
             if (file == null) {
                 return;
@@ -37,14 +44,21 @@ export default defineComponent({
                 this.userStore.updateAvatar(data.avatar);
         },
 
-        getAvatar() {
+        getAvatar(): string {
             if (this.userStore.user.avatar.startsWith("https://cdn.intra.42.fr/")) {
                 return this.userStore.user.avatar;
             }
             return this.URL + this.userStore.user.avatar;
         },
 
-        async changeUsername() {
+        isInputValid(): boolean {
+            return this.tmpName.length <= 20 && this.tmpName.length >= 3 && /^[a-zA-Z0-9_]*$/.test(this.tmpName);
+        },
+
+        async changeUsername(): Promise<void> {
+            if (!this.isInputValid()) {
+                return;
+            }
             this.nameEdit = false;
             await axios.patch("http://localhost:3000/users/" + this.userStore.user.id, {
                 username: this.tmpName
@@ -60,6 +74,11 @@ export default defineComponent({
                 }
                 this.tmpName = "";
             });
+        },
+
+        cancelEdit(): void {
+            this.nameEdit = false;
+            this.tmpName = "";
         },
     },
 
@@ -89,10 +108,11 @@ export default defineComponent({
                 Please type your new username
             </v-card-title>
             <v-card-text>
-                <v-form>
-                    <v-text-field label="New username" v-model="tmpName" required
+                <v-form @submit.prevent="changeUsername">
+                    <v-text-field label="New username" v-model="tmpName" :rules="rules"
                         @keydown.enter.prevent="changeUsername"></v-text-field>
-                    <v-btn color="primary" @click.stop="changeUsername">Save</v-btn>
+                    <v-btn color="primary" type="submit">Save</v-btn>
+                    <v-btn color="warning" @click="cancelEdit">Cancel</v-btn>
                 </v-form>
             </v-card-text>
         </v-card>
