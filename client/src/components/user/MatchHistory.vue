92c28623f8ff2ae6af16eaae8ae1fb3800cfcf13
borntobesso <soyoung.jung13@gmail.com>
2023-05-08 17:27:10 +0200

    added getAvatrar method in MatchHistory.vue for displaying uploaded image of opponents

--- client/src/components/user/MatchHistory.vue
+++ client/src/components/user/MatchHistory.vue
@@ -4,12 +4,12 @@ import { userStore } from "@/stores/user";
 import axios from "axios";
 
 export default defineComponent({
-  name: "MatchHistory",
+	name: "MatchHistory",
 
 	data() {
 		return {
 			avatar: {
-			size: "50",
+				size: "50",
 			},
 			userStore,
 
@@ -18,11 +18,12 @@ export default defineComponent({
 			opponents_ids: [],
 			opponents_name: [],
 			opponents_avatar: [],
+			URL: "http://localhost:3000/users/avatar/",
 		};
 	},
 
 	created() {
-		this.fetch()	
+		this.fetch()
 	},
 
 	methods: {
@@ -31,15 +32,14 @@ export default defineComponent({
 			const history_api = "http://localhost:3000/history/";
 
 			await axios.get(history_api + id, { withCredentials: true })
-			.then((response) => {
-				this.history = response.data;
-			})
-			.catch((error) => {
-				console.log(error);
-			})
+				.then((response) => {
+					this.history = response.data;
+				})
+				.catch((error) => {
+					console.log(error);
+				})
 
-			for (const item of this.history)
-			{
+			for (const item of this.history) {
 				const opponent_id = item.player1 == this.userStore.user.id ? item.player2 : item.player1;
 				this.opponents_ids.push(opponent_id);
 			}
@@ -50,21 +50,26 @@ export default defineComponent({
 
 			const users_api = "http://localhost:3000/users";
 			await axios.get(users_api, { withCredentials: true })
-			.then((response) => {
-				this.users = response.data
-			})
-			.catch((error) => {
-				console.log(error)
-			})
+				.then((response) => {
+					this.users = response.data
+				})
+				.catch((error) => {
+					console.log(error)
+				})
 
-			for (const item of this.opponents_ids)
-			{
+			for (const item of this.opponents_ids) {
 				const opponent = this.users.find((user) => user.id == item);
 				this.opponents_name.push(opponent.username);
-				this.opponents_avatar.push(opponent.avatar);
+				this.opponents_avatar.push(this.getAvatar(opponent.avatar));
 			}
 
 
+		},
+		getAvatar(avatar: string): string {
+			if (avatar.startsWith("https://cdn.intra.42.fr/")) {
+				return avatar;
+			}
+			return this.URL + avatar;
 		},
 	},
 
@@ -75,60 +80,57 @@ export default defineComponent({
 
 <template>
 	<div style="overflow-y: scroll; height: 65%; min-height: 950px;">
-	<v-container>
-		<h1 class="d-flex justify-center align-center">Match history</h1>
-		<v-card>
-			<v-row>
-				<v-col cols="3" class="column my-2">Opponent</v-col>
-				<v-col cols="3" class="column my-2">Score</v-col>
-				<v-col cols="3" class="column my-2" >Result</v-col>
-				<v-col cols="3" class="column my-2">Date</v-col>
-			</v-row>
-		</v-card>
-
-	<div>
-		<div 
-			v-for="(item, index) in this.history"
-			v-motion-fade
-			:delay="(index + 1) * 100">
+		<v-container>
+			<h1 class="d-flex justify-center align-center">Match history</h1>
 			<v-card>
-			<v-row class="ma-2">
-				<v-col cols="3" class="column">
-					<v-avatar>
-						<v-img :src="this.opponents_avatar[index]"></v-img>
-					</v-avatar>
-					<div class="ml-3">
-						{{ opponents_name[index] }}
-					</div>
-				</v-col>
-
-				<v-col cols="3" class="column">
-					{{ this.history[index].player1_score }} - {{ this.history[index].player2_score }}
-				</v-col>
-
-				<v-col cols="3" class="column">
-					{{ this.history[index].winner == this.userStore.user.id ? "win" : "lose" }}
-				</v-col>
-
-				<v-col cols="3" class="column">
-					{{ this.history[index].date }}
-				</v-col>
-			</v-row>
+				<v-row>
+					<v-col cols="3" class="column my-2">Opponent</v-col>
+					<v-col cols="3" class="column my-2">Score</v-col>
+					<v-col cols="3" class="column my-2">Result</v-col>
+					<v-col cols="3" class="column my-2">Date</v-col>
+				</v-row>
 			</v-card>
-		</div>
-	</div>
-	</v-container>
+
+			<div>
+				<div v-for="(item, index) in this.history" v-motion-fade :delay="(index + 1) * 100">
+					<v-card>
+						<v-row class="ma-2">
+							<v-col cols="3" class="column">
+								<v-avatar>
+									<v-img :src="this.opponents_avatar[index]"></v-img>
+								</v-avatar>
+								<div class="ml-3">
+									{{ opponents_name[index] }}
+								</div>
+							</v-col>
+
+							<v-col cols="3" class="column">
+								{{ this.history[index].player1_score }} - {{ this.history[index].player2_score }}
+							</v-col>
+
+							<v-col cols="3" class="column">
+								{{ this.history[index].winner == this.userStore.user.id ? "win" : "lose" }}
+							</v-col>
+
+							<v-col cols="3" class="column">
+								{{ this.history[index].date }}
+							</v-col>
+						</v-row>
+					</v-card>
+				</div>
+			</div>
+		</v-container>
 	</div>
 </template>
 
 <style scoped>
-
-.scroll{
+.scroll {
 	height: 100%;
 	overflow-y: scroll;
 	scrollbar-color: rebeccapurple green;
- 	scrollbar-width: thin;
+	scrollbar-width: thin;
 }
+
 .column {
 	display: flex;
 	justify-content: center;
@@ -147,5 +149,4 @@ export default defineComponent({
 	align-items: center;
 	margin: 3em 3em auto 3em;
 }
-
 </style>
